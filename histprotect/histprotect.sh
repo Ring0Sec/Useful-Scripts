fad0728add399547b3c44abbf1670b388587c8a2
Michael <mike-bailey@users.noreply.github.com>
2014-10-02 08:43:16 -0400

    Chmod it
    Set it to proper permissions

--- histprotect/histprotect.sh
+++ histprotect/histprotect.sh
@@ -26,7 +26,8 @@ chmod 750 ksh
 
 echo Protecting any other users on the system
 for i in $(find / -type f \( -name .bash_history -o -name .bashrc \)); do
-if [ -f $i ] ; then chattr +a $i ; fi
+if [ -f $i ] ; then  chmod 600 $i; chattr +a $i ; fi
+
 done
 for i in $(find / -name .bashrc); do
  echo "readonly PROMPT_COMMAND=\"history -a\"" >> $i
@@ -37,3 +38,6 @@ echo readonly HISTCMD  >> $i
 echo readonly HISTCONTROL >> $i
 echo readonly HISTIGNORE >> $i
 done
+
+echo "for i in \$(find / -type f \( -name .bash_history \)); do cat $i | grep \"history -c\" ; done >> /dev/null" >> /root/.bashrc
+if [ $? -eq 1 ] ; then echo History clear detected; fi
