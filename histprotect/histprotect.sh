5b93e0097688371187ebcfb4b6fa63ea72b07718
Michael <mike-bailey@users.noreply.github.com>
2014-10-02 07:57:27 -0400

    Secure root
    In the future:
    iterate through ls /home/, apply it to all of those
    then in the further future:
    ensure it exists

--- histprotect/histprotect.sh
+++ histprotect/histprotect.sh
@@ -1,2 +1,23 @@
 #/bin/bash
 echo HistProtect at le service!
+echo Straight up stolen from StackOverflow
+echo But it gets the job done so why would I rewrite it
+echo Making append-only
+chattr +a /root/.bash_history 
+chattr +a /root/.bash_profile 
+chattr +a /root/.bash_login 
+chattr +a /root/.profile 
+chattr +a /root/.bash_logout 
+chattr +a /root/.bashrc
+echo Hardening environment variables
+shopt -s histappend 
+ echo "readonly PROMPT_COMMAND=\"history -a\"" >> /root/.bashrc
+echo readonly HISTFILE >> /root/.bashrc
+echo readonly HISTFILESIZE  >> /root/.bashrc
+echo readonly HISTSIZE  >> /root/.bashrc
+echo readonly HISTCMD  >> /root/.bashrc
+echo readonly HISTCONTROL >> /root/.bashrc
+echo readonly HISTIGNORE >> /root/.bashrc
+chmod 750 csh 
+chmod 750 tcsh 
+chmod 750 ksh
