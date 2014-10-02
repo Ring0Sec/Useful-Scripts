495cb2c7fb06bb7af6f629b304186b14545f0662
Michael <mike-bailey@users.noreply.github.com>
2014-10-02 10:04:46 -0400

    Update histprotect.sh

--- histprotect/histprotect.sh
+++ histprotect/histprotect.sh
@@ -5,6 +5,11 @@ echo Straight up stolen from StackOverflow, then improved
 echo MOST of this is tested
 echo But it gets the job done so why would I rewrite it
 echo Making append-only
+echo Protecting any other users on the system
+for i in $(find / -type f \( -name .bash_history -o -name .bashrc \)); do
+if [ -f $i ] ; then  chmod 600 $i; chattr +a $i ; fi
+
+done
 if [ -f /root/.bash_history ] ; then chattr +a /root/.bash_history ; fi
 if [ -f /root/.bash_profile ] ; then chattr +a /root/.bash_profile  ; fi
 if [ -f /root/.bash_login ] ; then chattr +a /root/.bash_login  ; fi
@@ -24,11 +29,7 @@ chmod 750 csh
 chmod 750 tcsh 
 chmod 750 ksh
 
-echo Protecting any other users on the system
-for i in $(find / -type f \( -name .bash_history -o -name .bashrc \)); do
-if [ -f $i ] ; then  chmod 600 $i; chattr +a $i ; fi
 
-done
 for i in $(find / -name .bashrc); do
  echo "readonly PROMPT_COMMAND=\"history -a\"" >> $i
 echo readonly HISTFILE >> $i
